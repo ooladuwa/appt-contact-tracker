7b8db61064e7fc071467cb92524b8afbf4cba037
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-05-13 18:21:47 -0400

    add state to ContactsPage. Create handleSubmit to check for duplicate contacts

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
+const initialContact = {
+  id: Date.now(),
+  name: "Bob DaBuilda",
+  phone: 4044444444,
+  email: "bob@bob.com",
+};
+
 function App() {
   //Define state variables for contacts and appointments
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(initialContact);
   const [appointments, setAppointments] = useState([]);
 
   const ROUTES = {
@@ -23,6 +30,7 @@ function App() {
       newContact.email,
     ]);
   };
+  console.log(contacts);
 
   let newAppointment = (title, contact, date, time) => {
     setAppointments([
@@ -51,11 +59,15 @@ function App() {
           </Route>
           <Route path={ROUTES.CONTACTS}>
             {/* Add props to ContactsPage */}
-            <ContactsPage />
+            <ContactsPage contacts={contacts} newContact={newContact} />
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
             {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
+            <AppointmentsPage
+              appointments={appointments}
+              contacts={contacts}
+              newAppointment={newAppointment}
+            />
           </Route>
         </Switch>
       </main>
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,32 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ContactForm } from "../../components/contactForm/ContactForm";
+import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+export const ContactsPage = (props) => {
+  const { contacts, newContact } = props;
+
+  // Define state variables for contact info and duplicate check
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [duplicateCheck, setDuplicateCheck] = useState(false);
+
+  //   useEffect(() => {
+  //   setDuplicateCheck(
+  //   )
+  // })
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
+    contacts.forEach((contact) => {
+      if (contact.name === newContact.name) {
+        return alert("This contact already exists");
+      } else {
+        return contacts.push(newContact);
+      }
+    });
   };
 
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
+  // Add contact info and clear data if the contact name is not a duplicate
+
+  // Using hooks, check for contact name in the contacts array variable in props
 
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <ContactForm
+          name={name}
+          phone={phone}
+          email={email}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList contacts={contacts} />
       </section>
     </div>
   );
